b15667d5562d70c363d59d3c8b8f62c0982f8042
HighNoon_laptop <h1gh-noon@outlook.com>
2023-11-09 13:05:47 +0800

    HnCalendar Demo

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # your-uniapp
 
 **内置日历组件** `src/pages/components/hn-calendar`
+[预览✈️](https://h1gh-noon.github.io/your-uniapp/#/pages/demo/HnCalendar)
---
+++ src/pages/demo/HnCalendar.vue
@@ -0,0 +1,20 @@
+<template>
+  <hnCalendar v-model="data.dateArr" />
+  {{ data.date }}
+</template>
+
+<script setup name="HnCalendar">
+import { reactive, ref } from 'vue'
+import { onLoad } from '@dcloudio/uni-app'
+import hnCalendar from '@/components/hn-calendar'
+
+const data = reactive({
+  dateArr: [],
+  date: '2020-01-01'
+})
+onLoad(val => {
+  console.log(val)
+})
+</script>
+
+<style scoped lang="scss"></style>
